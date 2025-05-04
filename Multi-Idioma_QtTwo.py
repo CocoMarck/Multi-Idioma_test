23910f873eff13a5f74ec48db40842e341abf68a
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2025-05-03 21:23:18 -0600

    Add files via upload

--- Multi-Idioma_QtTwo.py
+++ Multi-Idioma_QtTwo.py
@@ -5,6 +5,8 @@ from PyQt6.QtWidgets import (
     QDialog,
     QPushButton,
     QLabel,
+    QComboBox,
+    QLineEdit,
     QVBoxLayout,
     QHBoxLayout
 )
@@ -17,61 +19,142 @@ from interface import Modulo_Util_Qt as Util_Qt
 class Window_Main(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setWindowTitle( get_text('app') )
         self.resize( nums_win_main[0], nums_win_main[1] )
         
         # Contenedor Principal
         vbox_main = QVBoxLayout()
         self.setLayout(vbox_main)
         
-        # Secciones Verticales, botones de opcion
-        self.button_lang = QPushButton( get_text('lang') )
-        #self.button_lang.clicked.connect(self.set_lang)
-        vbox_main.addWidget(self.button_lang)
-        
-        self.button_see_text = QPushButton( get_text('see-text') )
-        self.button_see_text.clicked.connect(self.see_text)
-        vbox_main.addWidget(self.button_see_text)
-        
-        # VBox, separacion de botones de opcion
-        vbox_main.addStretch()
-        
-        # Seccion vertical final, Boton agregar y remover
+        # Secciones Vertical, Seleccionar lenguaje
         hbox = QHBoxLayout()
         vbox_main.addLayout(hbox)
         
-        self.label = QLabel( get_text('add-or-remove-tag') )
-        hbox.addWidget( self.label )
+        self.label_set_lang = QLabel()
+        hbox.addWidget(self.label_set_lang)
+        
+        self.combobox_lang = QComboBox()
+        self.combobox_lang.activated.connect(self.set_lang)
+        hbox.addWidget(self.combobox_lang)
+
+
+
+
+        # Seccion vertical, Boton agregar tag
+        hbox = QHBoxLayout()
+        vbox_main.addLayout(hbox)
+        
+        self.button_add_tag = QPushButton( )
+        self.button_add_tag.clicked.connect( self.add_tag )
+        hbox.addWidget( self.button_add_tag )
         
         hbox.addStretch()
         
-        self.button_add = QPushButton( get_text('add') )
-        self.button_add.clicked.connect( self.add_or_remove_tag )
-        hbox.addWidget( self.button_add )
+        self.label_tag = QLabel()
+        hbox.addWidget( self.label_tag )
+
+        self.entry_tag = QLineEdit()
+        hbox.addWidget( self.entry_tag )
         
-        self.button_remove = QPushButton( get_text('remove') )
-        hbox.addWidget( self.button_remove )
+        self.label_tag_value = QLabel()
+        hbox.addWidget( self.label_tag_value )
+
+        self.entry_tag_value = QLineEdit()
+        hbox.addWidget( self.entry_tag_value )
+        
+
+
+
+        # Sección vertical Boton remover
+        hbox = QHBoxLayout()
+        vbox_main.addLayout(hbox)
+        
+        self.button_rm_tag = QPushButton( )
+        hbox.addWidget( self.button_rm_tag )
+        
+        hbox.addStretch()
+        
+        self.combobox_tag = QComboBox( )
+        hbox.addWidget( self.combobox_tag )
+        
+
+
+
+        # VBox, separacion
+        vbox_main.addStretch()
+        
+
+
+
+        # Seccion vertical | Ver texto
+        self.button_see_text = QPushButton( )
+        self.button_see_text.clicked.connect(self.see_text)
+        vbox_main.addWidget(self.button_see_text)
         
         # Actualizar texto de widgets contenidos en la ventana main
+        self.start_combobox()
         self.update()
         
         # Fin, Mostrar todo
         self.show()
         
-    def update(self):
-        self.button_lang.setText( get_text('lang') )
-        self.button_see_text.setText( get_text('see-text') )
-        self.label.setText( get_text('add-or-remove-tag') )
-        self.button_add.setText( get_text('add') )
-        self.button_remove.setText( get_text('rm') )
+    def set_text(self):
+        '''
+        Establecer el texto de todos los widgets que necesitan texto.
+        '''
+        self.button_see_text.setText( get_text('see-text', get_lang()) )
+        self.button_rm_tag.setText( get_text('rm-tag', get_lang()) )
+        self.button_add_tag.setText( get_text('add-tag', get_lang()) )
+        self.label_set_lang.setText( get_text('set-lang', get_lang()) )
+        self.label_tag.setText( get_text('tag', get_lang()) )
+        self.label_tag_value.setText( get_text('value', get_lang()) )
+        self.setWindowTitle( get_text('app', get_lang()) )
     
-    def set_lang():
+    def start_combobox(self):
+        '''
+        Establecer opciones a los combobox.
+        '''
+        self.combobox_lang.clear()
+        index = 0
+        index_to_set = None
+        for lang in get_available_lang():
+            self.combobox_lang.addItem(lang)
+            if lang == get_lang() and index_to_set == None:
+                index_to_set = index
+            index += 1
+        if index_to_set != None:
+            self.combobox_lang.setCurrentIndex(index_to_set)
+            
+        self.combobox_tag.clear()
+        for tag in get_all_tag():
+            print(tag)
+            self.combobox_tag.addItem(tag)
+
+        
+    def update(self):
+        '''
+        Actualizar todos los datos posibles.
+        '''
+        self.set_text()
+        self.start_combobox()
+
+    
+    def set_lang(self):
         # Actualizar texto de widgets contenidos en la ventana main
+        print( self.combobox_lang.currentText() )
+        print( update_lang( self.combobox_lang.currentText() ) )
+        self.set_text()
         self.update()
-        
-        
-    def add_or_remove_tag(self):
-        print()
+    
+    def add_tag(self):
+        print( self.combobox_lang.currentText() )
+        instruction = insert_tag( 
+            self.entry_tag.text(), 
+            self.combobox_lang.currentText(), 
+            self.entry_tag_value.text() 
+        )
+        print(instruction)
+        self.entry_tag.clear()
+        self.entry_tag_value.clear()
         self.update()
 
     
@@ -91,6 +174,8 @@ class Window_Main(QWidget):
             edit=False,
             size=nums_win_textedit,
         ).exec()
+
+
 
 
 if __name__ == '__main__':
