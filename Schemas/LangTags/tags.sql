b2e423058e4f72ef99d7d507af355194c2fc1fb0
Jean Abraham <jeanabrahamchc@gmail.com>
2026-06-29 17:00:00 -0600

    tags

--- Schemas/LangTags/tags.sql
+++ Schemas/LangTags/tags.sql
@@ -3,6 +3,6 @@ CREATE TABLE tags (
     name          TEXT NOT NULL UNIQUE,
     created_at    TEXT,
     updated_at    TEXT,
-    deleted_at    TEXT
+    deleted_at    TEXT,
     is_active     INTEGER NOT NULL DEFAULT 1
 );
