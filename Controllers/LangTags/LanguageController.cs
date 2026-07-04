9d1e9eef649b94bd7e620d1b8f883aed42143b41
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-07-04 16:36:57 -0600

    controllers logic

---
+++ Controllers/LangTags/LanguageController.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+
+// Logger
+using Microsoft.Extensions.Logging;
+
+// Essentials
+using Repositories.LangTags;
+using Entities.LangTags;
+
+namespace Controllers.LangTags {
+    public class LanguageController
+    {
+        // Variables
+        private LanguageRepository _repository;
+        private ILogger _logger;
+        private LanguageEntity _entity;
+
+        // Constructor
+        public LanguageController(
+            LanguageRepository repository, LanguageEntity entity, ILogger logger)
+        {
+            _repository = repository;
+            _entity = entity;
+            _logger = logger;
+        }
+
+        // Methods
+        public void Create(string code){
+            try {
+                bool exists = _repository.ExistsByCode(code);
+                if (!exists){
+                    _repository.Insert(code, true);
+                    _logger.LogInformation($"Code inserted `{code}`");
+                }
+            } catch (Exception e){
+                _logger.LogError(e, "");
+            }
+        }
+
+        public void Update(int id, string code){
+            try {
+                bool exists = _repository.ExistsById(id);
+                if (!exists){
+                    _repository.Update(id, code, true);
+                    _logger.LogInformation($"Code updated `{code}`");
+                }
+            } catch (Exception e){
+                _logger.LogError(e, "");
+            }
+        }
+
+        public void Delete(int id){
+            try {
+                bool exists = _repository.ExistsById(id);
+                if (!exists){
+                    _repository.Deactivate(id);
+                    _logger.LogInformation($"Code deleted `{id}`");
+                }
+            } catch (Exception e){
+                _logger.LogError(e, "");
+            }
+        }
+
+        public void Activate(int id){
+            try {
+                bool exists = _repository.ExistsById(id);
+                if (!exists){
+                    _repository.Activate(id);
+                    _logger.LogInformation($"Code activated `{id}`");
+                }
+            } catch (Exception e){
+                _logger.LogError(e, "Error activating `{id}`");
+            }
+        }
+        public string? GetCode(int id){
+            return "";
+        }
+        public int? GetId(string code){
+            return 0;
+        }
+
+        public string[] GetColumnNames() {
+            try {
+                return _repository.Table.GetColumnNames();
+            } catch (Exception e)
+            {
+                return new string[0];
+            }
+        }
+
+        public List<string[]> GetRowValues(){
+            try {
+                return _repository.Table.GetRowValues();
+            } catch (Exception e)
+            {
+                return new List<string[]>();
+            }
+        }
+
+    }
+}
--- Core/Sqlite/StandardTable.cs
+++ Core/Sqlite/StandardTable.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 // SQLite
 using Microsoft.Data.Sqlite;
 
@@ -25,9 +27,38 @@ namespace Core.Sqlite {
         }
 
         public int CountRows(){
-            using var reader = _db.Query($"SELECT COUNT(*) FROM \"{_name}\";");
+            using SqliteDataReader reader = _db.Query($"SELECT COUNT(*) FROM \"{_name}\";");
             reader.Read();
             return reader.GetInt32(0);
         }
+
+        public int CountColumns(){
+            using SqliteDataReader reader = GetAllRows();
+            return reader.FieldCount;
+        }
+
+        public string[] GetColumnNames() {
+            using SqliteDataReader reader = GetAllRows();
+            var names = new string[reader.FieldCount];
+            for (int i = 0; i <names.Length; i++){
+                names[i] = reader.GetName(0);
+            }
+            return names;
+        }
+
+        public List<string[]> GetRowValues() {
+            using SqliteDataReader reader = GetAllRows();
+            var rows = new List<string[]>();
+            int cols = reader.FieldCount;
+            while (reader.Read()) {
+                var row = new string[cols];
+                for (int i = 0; i < cols; i++){
+                    row[i] = reader.IsDBNull(i) ? null : reader.GetValue(i).ToString();
+                }
+                rows.Add(row);
+            }
+            return rows;
+        }
+
     }
 }
